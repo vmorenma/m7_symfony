c20dbebb3b1be4afdfa48cd6e06bc772702e40eb
vmorenma <victor.moreno.maya@gmail.com>
2016-10-28 16:20:27 +0200

    Initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/nombre/{name}", name="app_default_index")
+     */
+    public function indexAction($name)
+    {
+        return new Response("Hello".$name);
+    }
+
+    /**
+     * @Route ("prueba1", name="app_default_pruebaVista")
+     */
+    public function pruebaVistaAction()
+    {
+        return $this->render(':default:vista1.html.twig');
+
+    }
+}
