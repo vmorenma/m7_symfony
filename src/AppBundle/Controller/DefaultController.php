e7ac932b06371d44ac0bfd15410a601c0c26861e
vmorenma <victor.moreno.maya@gmail.com>
2016-11-18 15:26:50 +0100

    default index

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,28 +4,15 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/nombre/{name}", name="app_default_index")
+     * @Route("/", name="app_default_index")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return new Response("Hello".$name);
-    }
-
-    /**
-     * @Route ("/prueba1", name="app_default_pruebaVista")
-     */
-    public function pruebaVistaAction()
-    {
-        return $this->render(':default:vista1.html.twig',[
-            'titulo'    => 'Mi página web',
-            'resultado' => '3',
-        ]);
-
+        return $this->render(':default:index.html.twig');
     }
 
 }
